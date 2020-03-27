b7544a90f99aeb311df8ec6026156b808a6d5166
Tobias G <31646748+Daikalos@users.noreply.github.com>
2020-03-27 10:02:11 +0100

    Code cleanup
    added comments for attributes
    fixed unclear comments
    adjusted values for better gameplay

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Collider))]
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController m_CharacterController;
@@ -9,22 +11,24 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 m_MoveDirection;
     private Vector3 m_HitNormal;
 
-    private bool m_IsGrounded = false;
-    private float m_Velocity = 0.0f;
+    private bool m_IsGrounded;
+    private float m_Velocity;
 
-    [Range(1.0f, 80.0f)]
+    [Header("Player Attributes")]
+    [Tooltip("Player's movement speed"), Range(1.0f, 80.0f)]
     public float m_Speed = 10.0f;
-    [Range(2.0f, 20.0f)]
+    [Tooltip("Initial speed when pressing jump"), Range(2.0f, 20.0f)]
     public float m_JumpSpeed = 5.0f;
-    [Range(2.0f, 40.0f)]
+    [Tooltip("Speed to decrease player velocity when in air"), Range(2.0f, 40.0f)]
     public float m_Gravity = 10.0f;
-    [Range(0.0f, 1.0f)]
-    public float m_SlideFriction = 0.3f;
-    [Range(1.0f, 3.5f)]
-    public float m_SlopeRayLength = 1.5f;
-    [Range(0.5f, 10.0f)]
-    public float m_SlopeForce = 5.0f;
 
+    [Header("Slope Attributes")]
+    [Tooltip("Friction when sliding down slopes"), Range(0.0f, 1.0f)]
+    public float m_SlideFriction = 0.3f;
+    [Tooltip("How far to look down from bottom of player"), Range(1.0f, 4.0f)]
+    public float m_SlopeRayLength = 1.9f;
+    [Tooltip("At which force to push down player onto slope"), Range(0.5f, 15.0f)]
+    public float m_SlopeForce = 9.0f;
 
     void Start()
     {
@@ -33,6 +37,9 @@ public class PlayerMovement : MonoBehaviour
 
         m_MoveDirection = Vector3.zero;
         m_HitNormal = Vector3.zero;
+
+        m_IsGrounded = false;
+        m_Velocity = 0.0f;
     }
 
     void Update()
@@ -43,10 +50,9 @@ public class PlayerMovement : MonoBehaviour
 
         m_MoveDirection = transform.right * x + transform.forward * z;
 
-        //Slide
+        //If ground is tilted below slopelimit or not
         m_IsGrounded = (Vector3.Angle(Vector3.up, m_HitNormal) <= m_CharacterController.slopeLimit);
 
-        //Jump
         if (m_CharacterController.isGrounded)
         {
             m_Velocity = 0.0f;
@@ -59,6 +65,7 @@ public class PlayerMovement : MonoBehaviour
             }
             else
             {
+                //Move in direction of the given normal
                 m_MoveDirection.x += (1.0f - m_HitNormal.y) * m_HitNormal.x * (1.0f - m_SlideFriction);
                 m_MoveDirection.z += (1.0f - m_HitNormal.y) * m_HitNormal.z * (1.0f - m_SlideFriction);
             }
