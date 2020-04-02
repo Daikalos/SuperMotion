b48e5ac9da53645f5e0d3ed4c8095bd6dc15014d
Philip Forsner <ph.forsner@hotmail.com>
2020-04-02 14:02:38 +0200

    Nivåtest

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
