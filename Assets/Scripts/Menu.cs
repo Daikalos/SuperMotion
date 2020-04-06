d465ef8cd333fe3424664ae3575d815add0b26df
Bag <albinbagge@hotmail.com>
2020-04-07 01:01:33 +0200

    Added main menu and crosshair
    Lade till en meny med text, knappar och bilder. Sakerna i menyn är mer eller mindre placeholder.
    
    I samband med UI:n lades TMP till för bättre text. (~70 ändrade filer.)
    
    Gjorde ett "crosshair". En punkt i mitten av skärmen som ser ut som i Mirror's Edge Catalyst.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayLevel1() { SceneManager.LoadScene(0); }
+    public void PlayLevel2() { SceneManager.LoadScene(1); }
+    public void PlayLevel3() { SceneManager.LoadScene(2); }
+
+    // Developer levels
+    public void PlayAlbin() { SceneManager.LoadScene("Albin_Scene"); }
+    public void PlayPhilip() { SceneManager.LoadScene("Philip_Scene"); }
+    public void PlayTinea() { SceneManager.LoadScene("Tinea_Scene"); }
+    public void PlayTobias() { SceneManager.LoadScene("Tobias_Scene"); }
+    public void PlayYashi() { SceneManager.LoadScene("Yashi_Scene"); }
+
+    public void QuitGame() { Application.Quit(); }
+}
