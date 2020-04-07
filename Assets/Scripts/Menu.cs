6e9b9d76b44c7ed99803749b5c048e87276ec422
Bag <albinbagge@hotmail.com>
2020-04-07 09:50:28 +0200

    Added main menu scene

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public void PlayLevel1() { SceneManager.LoadScene(0); }
-    public void PlayLevel2() { SceneManager.LoadScene(1); }
-    public void PlayLevel3() { SceneManager.LoadScene(2); }
+    public void PlayLevel1() { SceneManager.LoadScene(1); }
+    public void PlayLevel2() { SceneManager.LoadScene(2); }
+    public void PlayLevel3() { SceneManager.LoadScene(3); }
 
     // Developer levels
     public void PlayAlbin() { SceneManager.LoadScene("Albin_Scene"); }
