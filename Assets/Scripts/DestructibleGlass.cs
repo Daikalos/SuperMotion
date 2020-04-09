a8bb946f2d7bdfd124b564a81629a7f670787288
Tobias G <31646748+Daikalos@users.noreply.github.com>
2020-04-10 00:01:18 +0200

    Added breakable glass

---
+++ Assets/Scripts/DestructibleGlass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleGlass : MonoBehaviour
+{
+    [SerializeField, Tooltip("Force the glass shatters from impact point"), Range(0.0f, 800.0f)]
+    private float m_ShatterForce = 300.0f;
+
+    public GameObject m_DestroyedVersion;
+
+    public void Shatter(Vector3 hitDirection, RaycastHit rayHit)
+    {
+        GameObject newObject = Instantiate(m_DestroyedVersion, transform.position, transform.rotation) as GameObject;
+        newObject.transform.localScale = transform.localScale;
+
+        for (int i = 0; i < newObject.transform.childCount; i++)
+        {
+            Rigidbody childRigidBody = newObject.transform.GetChild(i).GetComponent<Rigidbody>();
+
+            childRigidBody.AddForce(
+                (hitDirection * m_ShatterForce) + 
+                (childRigidBody.transform.position - rayHit.point) * (1.0f / (childRigidBody.transform.position - rayHit.point).magnitude) * m_ShatterForce);
+
+            Destroy(newObject.transform.GetChild(i).GetComponent<Rigidbody>(), 4.0f);
+            Destroy(newObject.transform.GetChild(i).GetComponent<Collider>(), 4.0f);
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerStrength.cs
+++ Assets/Scripts/PlayerStrength.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerStrength
 {
     private readonly PlayerMovement m_PlayerMovement;
+    private readonly Transform m_CameraTransform;
 
     public PlayerStrength(GameObject playerObject)
     {
         m_PlayerMovement = playerObject.GetComponent<PlayerMovement>();
+        m_CameraTransform = playerObject.GetComponentInChildren<Camera>().transform;
     }
 
     /// <summary>
@@ -16,6 +18,15 @@ public class PlayerStrength
     /// </summary>
     public void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(m_CameraTransform.position, m_CameraTransform.forward, out RaycastHit objectHit, 5.0f))
+            {
+                if (objectHit.collider.tag == "Glass")
+                {
+                    objectHit.collider.GetComponent<DestructibleGlass>().Shatter(m_CameraTransform.forward, objectHit);
+                }
+            }
+        }
     }
 }
