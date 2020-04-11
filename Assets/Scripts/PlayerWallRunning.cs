46c20a8b28174b43e4a3977eff92585d351cb344
Tobias G <31646748+Daikalos@users.noreply.github.com>
2020-04-11 23:15:20 +0200

    wall running

---
+++ Assets/Scripts/PlayerWallRunning.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallRunning : MonoBehaviour
+{
+    [SerializeField, Tooltip("How far to look for a wall"), Range(0.0f, 2.0f)]
+    private float m_RayLength = 0.05f;
+    [SerializeField, Tooltip("Running max point"), Range(0.0f, 20.0f)]
+    private float m_RunningHeight = 2.5f;
+    [SerializeField, Tooltip("Speed to decrease player fall velocity when wall running"), Range(-80.0f, 0.0f)]
+    private float m_Gravity = -15.0f;
+
+    private CharacterController 
+        m_CharacterController;
+    private PlayerMovement 
+        m_PlayerMovement;
+    private Vector3
+        m_Velocity,
+        m_PlayerVelocity,
+        m_LastPos;
+    private RaycastHit 
+        m_WallHit;
+    private float
+        m_WallCheckLength;
+    private bool
+        m_WallFound;
+
+    void Start()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+        m_PlayerMovement = GetComponent<PlayerMovement>();
+
+        m_LastPos = transform.position;
+        m_WallCheckLength = 15.0f;
+        m_WallFound = true;
+    }
+
+    void Update()
+    {
+        m_PlayerVelocity = transform.position - m_LastPos;
+
+        WallCollision();
+
+        if (CanWallRun())
+        {
+            if (m_PlayerMovement.enabled)
+            {
+                m_PlayerMovement.enabled = false;
+                m_PlayerMovement.Velocity = Vector3.zero;
+
+                //Push player towards the wall
+                Plane wallPlane = new Plane(m_WallHit.normal, m_WallHit.point);
+                m_CharacterController.Move(-m_WallHit.normal * (wallPlane.GetDistanceToPoint(transform.position) - m_CharacterController.radius));
+
+                m_Velocity.y = Mathf.Sqrt(m_RunningHeight * -2.0f * m_Gravity);
+            }
+
+            WallRun();
+        }
+        else
+        {
+            CheckForWall();
+
+            m_PlayerMovement.enabled = true;
+            m_LastPos = transform.position;
+        }
+    }
+
+    private void WallCollision()
+    {
+        m_WallHit = new RaycastHit();
+        float wallDistance = Mathf.Infinity;
+
+        //Cast rays in multiple directions using the player's look direction as reference
+        for (int i = -2; i <= 2; i++)
+        {
+            Debug.DrawRay(transform.position, Quaternion.AngleAxis(60.0f * i, Vector3.up) * transform.forward * (m_CharacterController.radius + m_RayLength));
+
+            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(60.0f * i, Vector3.up) * transform.forward, out RaycastHit objectHit, m_CharacterController.radius + m_RayLength))
+            {
+                //If wall is perpendicular to the ground and player is moving alongside the wall
+                if (Vector3.Dot(objectHit.normal, Vector3.up) == 0 && Vector3.Dot(m_PlayerVelocity, transform.forward) > 0)
+                {
+                    if (objectHit.distance < wallDistance)
+                    {
+                        wallDistance = objectHit.distance;
+                        m_WallHit = objectHit;
+                    }
+                }
+            }
+        }
+    }
+
+    private void WallRun()
+    {
+        //Find what direction to move; 1 = left, -1 = right
+        Vector3 relativePoint = transform.InverseTransformPoint(m_WallHit.point);
+        int moveDirection = (relativePoint.x < 0.0f) ? 1 : -1;
+        
+        m_CharacterController.Move(Vector3.Cross(m_WallHit.normal, Vector3.up) * moveDirection * m_PlayerMovement.Speed * Time.deltaTime);
+
+        m_Velocity.y += m_Gravity * Time.deltaTime;
+        m_CharacterController.Move(m_Velocity * Time.deltaTime);
+    }
+
+    private void CheckForWall()
+    {
+        m_WallFound = false;
+
+        //Player has to be looking at wall to be able to wall run
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit objectHit, m_CharacterController.radius + m_WallCheckLength))
+        {
+            if (Vector3.Dot(objectHit.normal, Vector3.up) == 0)
+            {
+                if (objectHit.collider != null && objectHit.collider == m_WallHit.collider)
+                {
+                    m_WallFound = true;
+                }
+            }
+        }
+    }
+
+    private bool CanWallRun()
+    {
+        return
+            m_WallHit.collider != null &&
+            m_WallFound &&
+            !m_CharacterController.isGrounded &&
+            !m_PlayerMovement.CanJump &&
+            m_PlayerMovement.Velocity.y >= 0.0f;
+    }
+}
