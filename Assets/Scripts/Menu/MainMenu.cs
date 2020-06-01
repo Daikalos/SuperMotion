5d0634f90517ee2db6070e1d3c2178ddf9c8da5c
Tobias G <31646748+Daikalos@users.noreply.github.com>
2020-06-01 15:43:52 +0200

    Improved Performance
    - Also renamed level_1 to level_2 and level_2 to level_1

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -8,13 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayLevel1() { SceneManager.LoadScene("Level_1"); }
     public void PlayLevel2() { SceneManager.LoadScene("Level_2"); }
     public void PlayLevel3() { SceneManager.LoadScene("Level_3"); }
-
-    // Developer levels
-    public void PlayAlbin() { SceneManager.LoadScene("Albin_Scene"); }
-    public void PlayPhilip() { SceneManager.LoadScene("Philip_Scene"); }
-    public void PlayTinea() { SceneManager.LoadScene("Tinea_Scene"); }
-    public void PlayTobias() { SceneManager.LoadScene("Tobias_Scene"); }
-    public void PlayYashi() { SceneManager.LoadScene("Yashi_Scene"); }
+    public void PlayTutorial() { SceneManager.LoadScene("Tutorial"); }
 
     public void QuitGame() { Application.Quit(); }
 }
